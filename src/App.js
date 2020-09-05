d786a5e42fb32c8081cdeb4279720462cf7f18ff
ahsanjamee <ahsanxami04@gmail.com>
2020-09-06 04:28:42 +0600

    ADD : Sidebar, dropdown, api request added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import SideDrawer from './components/SideDrawer/SideDrawer';
+import { FormControl, Select, MenuItem } from '@material-ui/core';
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+
+  useEffect( () => {
+    const getCountries = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => (
+          {
+            name: country.country,
+            value: country.countryInfo.iso2,
+            flag: country.countryInfo.flag
+          }
+        ))
+
+        setCountries(countries);
+      })
+    }
+
+    getCountries();
+  }, [])
+
+  //Selecting country from the dropdown
+  const onSelectCountry = (e) => {
+    const countryCode = e.target.value;
+    setCountry(countryCode);
+  }
+
   return (
     <div className="App">
-      hello
+      {console.log({countries})}
+      <div className="app_header">
+        <SideDrawer/>
+        <h1> Covid-19</h1>
+        <FormControl className="app_dropdown">
+          <Select 
+            variant="outlined" 
+            value={country}
+            onChange={onSelectCountry}
+          >
+          <MenuItem value="worldwide">WorldWide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}        
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
---
+++ src/components/SideDrawer/SideDrawer.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import FlagIcon from '@material-ui/icons/Flag';
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
+
+export default function SideDrawer() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    open: false
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+          <ListItem button>
+            <ListItemIcon><FlagIcon/></ListItemIcon>
+            <ListItemText primary= "Bangladesh" />
+          </ListItem>
+      </List>
+      <Divider />
+      <List>
+          <ListItem button>
+            <ListItemIcon><AccountCircleIcon/></ListItemIcon>
+            <ListItemText primary= "About Developer" onClick={()=> window.open("https://ahsanjamee.github.io", "_blank")}/>
+          </ListItem>
+      </List>
+    </div>
+  );
+
+  return (
+    <div>
+      {['left'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer( anchor , true)}><MenuIcon/></Button>
+          <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
